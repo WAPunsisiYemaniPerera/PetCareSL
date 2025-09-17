b02cb42155884aa9373483656014bbf7dbdde72d
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-09-18 00:59:01 +0530

    Add review submission endpoint to service routes

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -35,4 +35,45 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// adding reviews
+router.post('/:id/reviews', async (req, res) => {
+    // taking the ratings and comments from the frontend
+    const { rating, comment } = req.body;
+    const serviceId = req.params.id;
+
+    try {
+        const service = await Service.findById(serviceId);
+
+        if (service) {
+            // create new review object
+            // till we create user accounts, we get the name as 'Sample User'
+            const review = {
+                name: 'Sample User',
+                rating: Number(rating),
+                comment,
+            };
+
+            // adding new review to the existing review list of the service
+            service.reviews.push(review);
+
+            // save the changes in the database
+            await service.save();
+
+            res.status(201).json({
+                message: 'Review added successfully',
+            });
+
+        } else {
+            res.status(404).json({
+                message: 'Service not found',
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Server Error',
+        });
+    }
+});
+
 module.exports = router;
