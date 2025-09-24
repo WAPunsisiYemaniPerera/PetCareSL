ef2e9ee2041edf3221600342130c6633a03466e6
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-09-24 22:11:43 +0530

    Add CRUD operations for pets: create, retrieve, and delete functionalities

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -42,4 +42,49 @@ router.delete('/users/:id', async (req, res) => {
     }
 });
 
+router.post('/pets', async (req,res)=>{
+    try{
+        const pet = new Pet ({
+            name: "Buddy",
+            petType: "Dog",
+            breed: "Golden Retriever",
+            age: 3,
+            status: "For Adoption",
+            story: "A friendly and energetic dog looking for a loving home.",
+            shelterInfo: "Happy Tails Shelter, 1234 Pet Lane, Petville",
+            image: 'https://example.com/images/buddy.jpg',
+            user: req.user._id
+        })
+        const createdPet = await pet.save();
+        res.status(201).json(createdPet);
+    } catch (error){
+        res.status(500).json({ message: 'Server Error' });
+    }
+})
+
+//to see all the pets (owned and for adoption)
+router.get('/pets', async(req,res)=>{
+    try{
+        const pets = await Pet.find({});
+        res.json(pets);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+})
+
+//delete a pet
+router.delete('/pets/:id', async (req, res) => {
+    try{
+        const pet = await Pet.findById(req.params.id);
+        if(pet){
+            await Pet.deleteOne({_id: pet._id});
+            res.json({ message: 'Pet removed' });
+        } else {
+            res.status(404).json({ message: 'Pet not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+})
+
 module.exports = router;
